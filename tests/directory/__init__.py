23d3027243cff0345a258ea4187adbf1858e7ba5
speedyleion <speedyleion@users.noreply.github.com>
2016-07-10 14:58:29 -0700

    adding initial test skeleton

---
+++ tests/directory/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+"""
+:mod:`__init__` --- Package file
+===========================================
+Nose uses packages for test discovery so need an __init__.py file
+"""
---
+++ tests/directory/test_directory.py
@@ -0,0 +1,15 @@
+"""
+This will test the functionality of the directory and file node logic for the
+shared IDE files.
+
+"""
+
+import unittest
+
+
+class TestDirectory(unittest.TestCase):
+
+    """Docstring for TestDirectory. """
+
+    def test_tacos(self):
+        self.assertEquals(3, 4)
