a6e95b94d52e54de231f3c8172fad81ed98daa42
speedyleion <speedyleion@users.noreply.github.com>
2016-07-09 19:27:56 -0700

    working on directory class methods

--- python/directory.py
+++ python/directory.py
@@ -2,25 +2,64 @@
 Generic directory interface
 """
 
-import abc
-
 
 class Directory(object):
     """
-    This is an abstract class representing a directory of shared files for an
-    editor.  The idea is that this class provides a generic interface for
-    describing directory structure changes without having to know the nitty
-    gritty details of a specific editor interface.
+    This provides a generic directory interface for listing subdirectories and
+    files.
 
-    TODO: create image of interface
+    Directory paths are always deliminated with `/`.
 
     """
-    __metaclass__ = abc.ABCMeta
 
-    @abc.abstractmethod
-    def get_files(self):
+    def __init__(self, name, parent=None):
+        """Directory and its name
+
+        Args:
+            name (string): The directory name
+            parent (Directory): The parent directory.  Can be None if this is
+                                the root directory
+
         """
-        This will return the shared files provided by this interface.
+        self.name = name
+        self.parent = parent
+        self.sub_directories = []
+        self.files = []
+
+    def walk(self):
+        """
+        This is a similar interface to :meth:os.walk it always starts at the
+        root of this directory
 
         """
         return
+
+    def mkdir(self, path):
+        """Create subdirectory
+
+        Args:
+            path (string): The path to make directories for.
+
+        Raises OSError if the directory already exists
+
+        """
+        for sub_dir in self.sub_directories:
+            if sub_dir.name == path:
+                raise OSError('Subdirectory %s already exists' % path)
+
+        self.sub_directories.append(Directory(path, self))
+
+    def makedirs(self, path):
+        """Recursively creates subdirectories
+
+        Args:
+            path (string): Subdirectory tree to create
+
+        Raises OSError if the directory already exists
+
+        """
+        # This isn't ideal but more or less works for now
+        sub_dirs = path.split('/', 1)
+        self.mkdir(sub_dirs[0])
+        if len(sub_dirs) > 1:
+            self.sub_directories[-1].makedirs(sub_dirs[1])
